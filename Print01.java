58efe15d8a7aff572d6013700170d43e7e0b9f8c
D24gustn <162386334+D24gustn@users.noreply.github.com>
2024-03-06 18:14:42 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,14 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "석현수";
+		int myAge = 20;
+		System.out.println("이름 : " + myName);
+		System.out.println("나이 : " + myAge);
+		System.out.println("이름 : " + myName + " 나이 : " + myAge);
+	} 
+
+}
